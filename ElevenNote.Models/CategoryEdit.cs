9f7b7e5593c531e75ba0610645326071ac277f4a
Luke <lukereibel@gmail.com>
2021-03-09 18:23:33 -0500

    Add project files.

---
+++ ElevenNote.Models/CategoryEdit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class CategoryEdit
+    {
+
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters")]
+        [MaxLength(40, ErrorMessage = "there are too many characters")]
+        public string Name { get; set; }
+
+
+    }
+}
